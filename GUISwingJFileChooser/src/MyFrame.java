1368ec7b4d9197ca6ded1e60f14dea67a2b70b69
FerminBuenoPatduJr <rajubueno411@gmail.com>
2023-01-10 14:36:47 +0800

    save changes

--- GUISwingJFileChooser/src/MyFrame.java
+++ GUISwingJFileChooser/src/MyFrame.java
@@ -1,14 +1,26 @@
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame implements ActionListener{
 	
+	JButton button;
+	
 	MyFrame(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new FlowLayout());
+		
+		button = new JButton("Select File");
+		button.addActionListener(this);
+		
+		
+		
+		this.add(button);
 		this.pack();
 		this.setVisible(true);
 	}
@@ -16,6 +28,27 @@ public class MyFrame extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if (e.getSource()==button) {
+			
+			JFileChooser fileChooser = new JFileChooser();
+			
+			//fileChooser.setCurrentDirectory(new File("."));//save in project directory
+			fileChooser.setCurrentDirectory(new File("C:\\Users\\fpatdu\\Desktop"));//save on desktop
+			
+			
+			//System.out.println( fileChooser.showOpenDialog(null));//select file to open
+			
+			//int response = fileChooser.showOpenDialog(null);//select file to open
+			int response = fileChooser.showSaveDialog(null); //select file to save
+			
+			//if (response == 0) { //one way to filter
+			if (response== JFileChooser.APPROVE_OPTION) {
+				File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+				System.out.println(file);
+				
+			}
+			
+		}
 		
 	}
 
