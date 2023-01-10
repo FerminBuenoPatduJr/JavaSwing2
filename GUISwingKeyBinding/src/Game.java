d8f3e8c7a5d92b73ffea94ca35508384322632c3
FerminBuenoPatduJr <rajubueno411@gmail.com>
2023-01-10 20:11:14 +0800

    save changes

--- GUISwingKeyBinding/src/Game.java
+++ GUISwingKeyBinding/src/Game.java
@@ -25,6 +25,23 @@ public class Game {
 		label.setBounds( 100, 100, 100, 100);
 		label.setOpaque(true);
 		
+		upAction = new UpAction();
+		downAction = new DownAction();
+		leftAction = new LeftAction();
+		rightAction = new RightAction();
+		
+		label.getInputMap().put(KeyStroke.getKeyStroke("UP"), upAction);
+		label.getActionMap().put("upAction", upAction);
+		
+		label.getInputMap().put(KeyStroke.getKeyStroke("DOWN"), downAction);
+		label.getActionMap().put("downAction", downAction);
+		
+		label.getInputMap().put(KeyStroke.getKeyStroke("LEFT"), leftAction);
+		label.getActionMap().put("leftAction", leftAction);
+		
+		label.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), rightAction);
+		label.getActionMap().put("rightAction", rightAction);
+		
 		frame.add(label);
 		frame.setVisible(true);
 		
@@ -37,6 +54,7 @@ public class Game {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			label.setLocation(label.getX(), label.getY()-10);
 			
 		}
 		
@@ -47,6 +65,7 @@ public class Game {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			label.setLocation(label.getX(), label.getY()+10);
 			
 		}
 		
@@ -57,6 +76,7 @@ public class Game {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			label.setLocation(label.getX()-10, label.getY());
 			
 		}
 		
@@ -67,6 +87,7 @@ public class Game {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			label.setLocation(label.getX()+10, label.getY());
 			
 		}
 		
