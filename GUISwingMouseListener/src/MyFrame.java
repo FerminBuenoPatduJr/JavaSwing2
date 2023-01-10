0b32e63b3e28e1040000f4d27e220a2cd5913dbc
FerminBuenoPatduJr <rajubueno411@gmail.com>
2023-01-10 16:43:09 +0800

    save changes

---
+++ GUISwingMouseListener/src/MyFrame.java
@@ -0,0 +1,64 @@
+
+import java.awt.Color;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame implements MouseListener{
+	
+	JLabel label;
+	
+	MyFrame(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500,500);
+		this.setLayout(null);
+//		this.setLayout(new FlowLayout());
+//		this.pack();
+		
+		label = new JLabel();
+		label.setBounds(0, 0, 100, 100);
+		label.setBackground(Color.red);
+		label.setOpaque(true);
+		label.addMouseListener(this);
+		
+		
+		this.add(label);
+		this.setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		//System.out.println("You clicked the mouse!");
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		//System.out.println("You pressed the mouse!");
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		//System.out.println("You released the mouse!");
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("You entered the component.");
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("You exited the component.");
+		
+	}
+
+}
