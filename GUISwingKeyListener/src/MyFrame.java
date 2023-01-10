9278e0defd41fd5699c3b63d5eeff6dca65ad40a
FerminBuenoPatduJr <rajubueno411@gmail.com>
2023-01-10 15:02:25 +0800

    save project

---
+++ GUISwingKeyListener/src/MyFrame.java
@@ -0,0 +1,19 @@
+
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+	
+	MyFrame(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500,500);
+		this.setLayout(null);
+		this.setVisible(true);
+	}
+
+
+}
