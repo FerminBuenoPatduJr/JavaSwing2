2d2d3d7fcee9482c3d156ddb9562f5a304f212c0
FerminBuenoPatduJr <rajubueno411@gmail.com>
2023-01-10 16:12:59 +0800

    save changes add icon

--- GUISwingKeyListener/src/MyFrame.java
+++ GUISwingKeyListener/src/MyFrame.java
@@ -10,17 +10,24 @@ public class MyFrame extends JFrame implements KeyListener{
 	
 	JLabel label;
 	
+	ImageIcon icon;
+	
 	MyFrame(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500,500);
 		this.setLayout(null);
 		this.addKeyListener(this);
 		
+		icon = new ImageIcon("rocket1.png");
+		
 		label = new JLabel();
 		label.setBounds(0, 0,100,100);
-		label.setBackground(Color.red);
-		label.setOpaque(true);
+		label.setIcon(icon);
+		//label.setBackground(Color.red);
+		//label.setOpaque(true);
 		
+		
+		this.getContentPane().setBackground(Color.black);
 		this.add(label);
 		this.setVisible(true);
 	}
