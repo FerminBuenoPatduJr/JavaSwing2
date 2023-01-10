51869ccc340481cfe6262e940ecbab6f94ef8c22
FerminBuenoPatduJr <rajubueno411@gmail.com>
2023-01-10 09:32:43 +0800

    save changes

--- GUISwingJComboBox/src/MyFrame.java
+++ GUISwingJComboBox/src/MyFrame.java
@@ -13,10 +13,22 @@ public class MyFrame extends JFrame implements ActionListener{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new FlowLayout());
 		
+		
+		//note: use wrapper classes to store primitive types. e.g.: Integer, Double, Float, etc.
+		//see example below
+		//Integer[] number = {1,2,3};
 		String[] animals = {"dog", "cat", "bird","pig", "giraffe", "lion", "tiger"};
 		
 		comboBox = new JComboBox(animals);
 		comboBox.addActionListener(this);
+		//comboBox.setEditable(true);//set to editable
+		//System.out.println(comboBox.getItemCount());//get the total count of item/s
+		//comboBox.addItem("horse");//add item to the drop-down
+		//comboBox.insertItemAt("snake", 0);//insert item at specific index
+		//comboBox.setSelectedIndex(0);//set selected item at specified index
+		//comboBox.removeItem("cat");//remove specified item
+		//comboBox.removeItemAt(0);//remove item at specified index
+		//comboBox.removeAllItems();//remove all items
 		
 		this.add(comboBox);
 		this.pack();
@@ -27,7 +39,8 @@ public class MyFrame extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (e.getSource()==comboBox) {
-			System.out.println(comboBox.getSelectedIndex());
+			System.out.println(comboBox.getSelectedItem());//get the value
+			System.out.println(comboBox.getSelectedIndex());//get the index
 		}
 		
 	}
